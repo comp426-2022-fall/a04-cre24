29450b6a83d6e3b9c6cc731b4ebedfb8e6fb81c4
Ce'Niyah Ellison <69821183+cre24@users.noreply.github.com>
2022-11-17 23:14:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const args = minimist(process.argv.slice(2));
 const port = args.port ? args.port: 5000;
 
-// Send requests to api
+// Defining an endpoint
 app.get('/app', (req, res) => {
     res.send("200 OK");
 })
@@ -24,3 +24,23 @@ app.get('/app/roll/:sides', (req, res, next) => {
     res.send(Roll(parseInt(req.params.sides), 2, 1));
     res.end();
 })
+
+app.get('/app/roll/:sides/:dice', (req, res, next) => {
+    res.send(Roll(parseInt(req.params.sides), parseInt(req.params.dice). 1));
+    res.end();
+})
+
+app.use('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
+    res.send(Roll(parseInt(req.params.sides), parseInt(req,params.dice), parseInt(req.params.rolls)));
+    res.end();
+})
+
+// used same roll function variable names to stay consistent
+app.use('/app/roll', (req, res, next) => {
+    var numSides = req.params.sides ? parseInt(req.params.sides) : 6;
+    var numDice = req.params.dice ? parseInt(req.params.dice) : 2;
+    var numRolled = req.params.rolls ? parseInt(req.params.rolls) : 1;
+    res.send(Roll(numSides,numDice,numRolled));
+    res.end(); 
+})
+
