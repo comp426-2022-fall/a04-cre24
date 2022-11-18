0102a4c7bac66a65338d99c49757d74e3f967062
Ce'Niyah Ellison <69821183+cre24@users.noreply.github.com>
2022-11-17 23:03:12 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 #!/usr/bin/env node
 
+// Import statements
 import Roll from './lib/lib/roll.js';
 import express from 'express';
 import minimist from 'minimist';
+
+// const declarations
+const app = express();
+const args = minimist(process.argv.slice(2));
+const port = args.port ? args.port: 5000;
+
+// Send requests to api
+app.get('/app', (req, res) => {
+    res.send("200 OK");
+})
+
+app.get('/app/roll', (req, res, next) => {
+    res.send(Roll(6,2,1));
+    res.end();
+})
+
+app.get('/app/roll/:sides', (req, res, next) => {
+    res.send(Roll(parseInt(req.params.sides), 2, 1));
+    res.end();
+})
